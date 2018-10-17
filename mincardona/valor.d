540c97ba93e5e937964bff363477407e501256ea
Michael Incardona <michael.incardona@outlook.com>
2018-10-17 09:04:19 -0400

    Add some syntactic sugar functions

--- mincardona/valor.d
+++ mincardona/valor.d
@@ -52,12 +52,21 @@ class AndValor(T, L, R) : Valor!T
     }
 }
 
-Valor!T and(T = L, L, R)(Valor!L lhs, Valor!R rhs) {
+Valor!T v_gt(T = C, C)(C compareTo) {
+    return new GreaterThanValor!(T, C)(compareTo);
+}
+
+Valor!T v_lt(T = C, C)(C compareTo) {
+    return new LessThanValor!(T,C)(compareTo);
+}
+
+Valor!T v_and(T = L, L, R)(Valor!L lhs, Valor!R rhs) {
     return new AndValor!(T, L, R)(lhs, rhs);
 }
 
 int main(string[] args) {
-    auto v = new GreaterThanValor!(int,int)(0).and!int(new LessThanValor!(int,int)(2));
+    //auto v = new GreaterThanValor!(int,int)(0).v_and!int(new LessThanValor!(int,int)(2));
+    auto v = v_gt!int(0).v_and!int(v_lt!int(2));
     writeln(v.validate(-1));
     writeln(v.validate(0));
     writeln(v.validate(1));
